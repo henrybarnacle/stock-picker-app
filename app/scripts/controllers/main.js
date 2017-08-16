146701ffca5894de0474e0520dd68a04078b2519
Henry Barnacle <henrybarnacle@Henrys-MBP.fios-router.home>
2017-08-16 12:07:17 -0400

    built functionality to display on page instead of alert

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,5 +1,4 @@
 'use strict';
-
 /**
  * @ngdoc function
  * @name stockPickerAppApp.controller:MainCtrl
@@ -7,36 +6,45 @@
  * # MainCtrl
  * Controller of the stockPickerAppApp
  */
-angular.module('stockPickerAppApp')
-  .controller('MainCtrl', function ($scope) {
-
-$scope.stock = null;
-$scope.getQuandl = function() {
- 
-var symbol = $scope.stock;
-
-    var quandlUrl = 'https://www.quandl.com/api/v3/datasets/WIKI/' + symbol + '/data.json?&column_index=4api_key=axPmiG5ZrkVbD7Q82qvA';
-    $.ajax({
-      url: quandlUrl,
-      cache: true,
-      dataType: 'json'
-    }).done(function(results) {
-      var latest = results.dataset_data.data[0][1];
-      var pastWeek = results.dataset_data.data[5][1];
-      var pastMonth = results.dataset_data.data[20][1];
-      var past12Months = results.dataset_data.data[240][1];
-
-      var weekChange = (latest - pastWeek) / (pastWeek / 100);
-
-      var monthChange = (latest - pastMonth) / (pastMonth / 100);
-
-      var yearChange = (latest - past12Months) / (past12Months / 100);
- 
-      alert(symbol + " is priced at  " + latest + " as of last closing. This respresents a change of " + weekChange.toFixed(2) + "% over the past week, " + monthChange.toFixed(2) + "% over the past month and " + yearChange.toFixed(2) + "% over the past 12 months.");
-     }).fail(function(jqXHR, textStatus) {
-      alert('Nothing Found!');
-    });
-
-  };
-   
-}); 
+angular.module('stockPickerAppApp').controller('MainCtrl', function($scope) {
+	$scope.stock = null;
+	$scope.getQuandl = function() {
+		var latest = '';
+		var pastWeek = ''
+		var pastMonth = '';
+		var past12Months = '';
+		var weekChange = '';
+		var monthChange = '';
+		var yearChange = '';
+		var symbol = $scope.stock;
+		var quandlUrl = 'https://www.quandl.com/api/v3/datasets/WIKI/' + symbol + '/data.json?api_key=axPmiG5ZrkVbD7Q82qvA';
+		$.ajax({
+			url: quandlUrl,
+			cache: true,
+			dataType: 'json'
+		}).done(function(results) {
+			latest = results.dataset_data.data[0][1];
+			pastWeek = results.dataset_data.data[5][1];
+			pastMonth = results.dataset_data.data[20][1];
+			past12Months = results.dataset_data.data[240][1];
+			weekChange = (latest - pastWeek) / (pastWeek / 100);
+			monthChange = (latest - pastMonth) / (pastMonth / 100);
+			yearChange = (latest - past12Months) / (past12Months / 100);
+			console.log("1 done");
+		}).done(function(response) {
+			var aString = symbol + " is priced at $" + latest + " as of last closing.";
+			var bString = "This represents a change of:";
+			var cString = weekChange.toFixed(2) + "% over the past week.";
+			var dString = monthChange.toFixed(2) + "% over the past month.";
+			var eString = yearChange.toFixed(2) + "% over the past 12 months.";
+			$scope.a = aString;
+			$scope.b = bString;
+			$scope.c = cString;
+			$scope.d = dString;
+			$scope.e = eString;
+			$scope.$apply();
+		}).fail(function(jqXHR, textStatus) {
+			alert('Nothing Found!');
+		});
+	};
+});
